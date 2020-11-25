0949eb934854f00db7d149fcb291e1079d41d186
Han5991 <73056306+Han5991@users.noreply.github.com>
2020-11-25 20:21:14 +0900

    도시 자바 파일 수정

--- Burmabble/src/Bu/City.java
+++ Burmabble/src/Bu/City.java
@@ -2,19 +2,38 @@ package Bu;
 
 public class City {
 	private String name;
-	private Building building[];
-	private int passagemoney;
 	private String master;
+	private int inn;
+	private int motel;
+	private int hotel;
+	private int passagemoney;
 
-//	public void setpassagemoney() {
-//		this.passagemoney=building[0].
-//	}
+	public City(String name, String master) {
+		this.name = name;
+		this.master = master;
+		this.inn = 1000;
+		this.motel = 2000;
+		this.hotel = 3000;
+		this.passagemoney = 0;
+	}
 
 	public void setmaster(String master) {
 		this.master = master;
 	}
 
-	public void setname(String name) {
-		this.name = name;
+	public void setpassagemoney(int type, int number) {
+		switch (type) {
+		case 1:
+			passagemoney += inn * number;
+			break;
+		case 2:
+			passagemoney += motel * number;
+			break;
+		case 3:
+			passagemoney += hotel * number;
+			break;
+		default:
+			System.out.println("잘 못 된 접근입니다.");
+		}
 	}
 }
